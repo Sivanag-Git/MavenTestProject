8e17b604c578b944bbbb857d98f511f3dbb403c3
Sivanag-Git <42891893+Sivanag-Git@users.noreply.github.com>
2020-01-23 13:16:00 +0530

    Update TestClass.java

--- src/test/java/com/myMavenProject/mySeleniumTestProject/TestClass.java
+++ src/test/java/com/myMavenProject/mySeleniumTestProject/TestClass.java
@@ -1,5 +1,5 @@
 package com.myMavenProject.mySeleniumTestProject;
-
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,9 +15,13 @@ public class TestClass {
 	@BeforeMethod
 	public void launchDriver()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Drivers\\chromedriver.exe");
+		File file = new File("Drivers/chromedriver.exe"); 
+		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath()); 		
 		ChromeOptions chromeOptions= new ChromeOptions();
-		//chromeOptions.setBinary("C:\\Selenium\\Drivers\\chromedriver.exe");
+		chromeOptions.addArguments("disable-infobars"); // disabling infobars
+		chromeOptions.addArguments("--disable-extensions"); // disabling extensions
+		chromeOptions.addArguments("--disable-gpu"); // applicable to windows os only
+		chromeOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 		chromeOptions.addArguments("--headless");
 		driver = new ChromeDriver(chromeOptions);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
